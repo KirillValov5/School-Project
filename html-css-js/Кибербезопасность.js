a2cf9b4b623f18272ff023c58cde9e6c49248b8d
KirillValov5 <98196324+KirillValov5@users.noreply.github.com>
2023-01-15 20:00:00 +0700

    Create Кибербезопасность.js

---
+++ html-css-js/Кибербезопасность.js
@@ -0,0 +1,12 @@
+function show (hidden)
+{
+    var elem=document.getElementById(hidden);
+    if (elem)
+        elem.style.display="block";
+}
+function antishow (hidden)
+{
+    var elem=document.getElementById(hidden);
+    if (elem)
+        elem.style.display="none";
+}
